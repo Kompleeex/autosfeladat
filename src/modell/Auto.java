ad63a95091235d3415e8206097284e6a445d01b9
kompleeex <paaladam1@gmail.com>
2023-02-13 14:38:10 +0100

    elso commit jav2

--- src/modell/Auto.java
+++ src/modell/Auto.java
@@ -1,6 +1,22 @@
 
 package modell;
 
-public class Auto {
+public class Auto extends Jarmu{
     private boolean defekt;
+
+    public Auto(boolean defekt, boolean beinditva, boolean uzemanyag, boolean megerkezett) {
+        super(beinditva, uzemanyag, megerkezett);
+        this.defekt = defekt;
+    }
+    
+    public void kereketCserel(){
+
+    }
+    
+    @Override
+    public boolean halad(){
+        return false;
+    }
 }
+
+
--- src/modell/Jarmu.java
+++ src/modell/Jarmu.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modell;
 
-/**
- *
- * @author Acer
- */
-public class Jarmu {
+public abstract class Jarmu {
+    private boolean beinditva,uzemanyag,megerkezett;
+
+    public Jarmu() {
+        
+    }
+
+    public Jarmu(boolean beinditva, boolean uzemanyag, boolean megerkezett) {
+        this.beinditva = beinditva;
+        this.uzemanyag = uzemanyag;
+        this.megerkezett = megerkezett;
+    }
     
+    
+    public void beindit(){
+    
+    }
+    
+    public void leallit(){
+    
+    }
+    
+    public boolean tankol(){
+        return false;
+    }
+    
+    public boolean halad(){
+        return false;
+    }
 }
